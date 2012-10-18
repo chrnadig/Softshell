13d3bccb6748e00b35a988f3981cc48a297e3544
William Dillon <hpux735@gmail.com>
2012-10-18 11:00:53 -0700

    Added additional dongle signatures from the osmocom sources

--- rtl-sdr/rtl-sdr/RTLSDRDevice.m
+++ rtl-sdr/rtl-sdr/RTLSDRDevice.m
@@ -48,22 +48,30 @@ static rtlsdr_dongle_t known_devices[] = {
 	{ 0x0bda, 0x2838, "ezcap USB 2.0 DVB-T/DAB/FM dongle" },
 	{ 0x0ccd, 0x00a9, "Terratec Cinergy T Stick Black (rev 1)" },
 	{ 0x0ccd, 0x00b3, "Terratec NOXON DAB/DAB+ USB dongle (rev 1)" },
+	{ 0x0ccd, 0x00b4, "Terratec NOXON DAB/DAB+ USB dongle (rev 1)" },
+	{ 0x0ccd, 0x00b7, "Terratec NOXON DAB/DAB+ USB dongle (rev 1)" },
+	{ 0x0ccd, 0x00c6, "Terratec NOXON DAB/DAB+ USB dongle (rev 1)" },
 	{ 0x0ccd, 0x00d3, "Terratec Cinergy T Stick RC (Rev.3)" },
 	{ 0x0ccd, 0x00d7, "Terratec T Stick PLUS" },
 	{ 0x0ccd, 0x00e0, "Terratec NOXON DAB/DAB+ USB dongle (rev 2)" },
+	{ 0x185b, 0x0620, "Compro Videomate U620F"},
+	{ 0x185b, 0x0650, "Compro Videomate U650F"},
+	{ 0x185b, 0x0680, "Compro Videomate U680F"},
+	{ 0x1f4d, 0xa803, "Sweex DVB-T USB" },
+	{ 0x1f4d, 0xb803, "GTek T803" },
+	{ 0x1f4d, 0xc803, "Lifeview LV5TDeluxe" },
+	{ 0x1f4d, 0xd286, "MyGica TD312" },
+	{ 0x1f4d, 0xd803, "PROlectrix DV107669" },
+	{ 0x1b80, 0xd398, "Zaapa ZT-MINDVBZP" },
 	{ 0x1b80, 0xd3a4, "Twintech UT-40" },
+	{ 0x1d19, 0x1101, "Dexatek DK DVB-T Dongle (Logilink VG0002A)" },
+	{ 0x1d19, 0x1102, "Dexatek DK DVB-T Dongle (MSI DigiVox mini II V3.0)" },
+	{ 0x1d19, 0x1103, "Dexatek Technology Ltd. DK 5217 DVB-T Dongle" },
+	{ 0x0458, 0x707f, "Genius TVGo DVB-T03 USB dongle (Ver. B)" },
 	{ 0x1b80, 0xd393, "GIGABYTE GT-U7300" },
 	{ 0x1b80, 0xd394, "DIKOM USB-DVBT HD" },
 	{ 0x1b80, 0xd395, "Peak 102569AGPK" },
 	{ 0x1b80, 0xd39d, "SVEON STV20 DVB-T USB & FM" },
-	{ 0x1d19, 0x1101, "Dexatek DK DVB-T Dongle (Logilink VG0002A)" },
-	{ 0x1d19, 0x1102, "Dexatek DK DVB-T Dongle (MSI DigiVox mini II V3.0)" },
-	{ 0x1d19, 0x1103, "Dexatek Technology Ltd. DK 5217 DVB-T Dongle" },
-	{ 0x185b, 0x0620, "Compro Videomate U620F"},
-	{ 0x185b, 0x0650, "Compro Videomate U650F"},
-	{ 0x1f4d, 0xb803, "GTek T803" },
-	{ 0x1f4d, 0xc803, "Lifeview LV5TDeluxe" },
-	{ 0x0458, 0x707f, "Genius TVGo DVB-T03 USB dongle (Ver. B)" },
 };
 
 enum usb_reg {
@@ -973,7 +981,7 @@ static dispatch_once_t onceToken;
 
 -(double)setSampleRate:(double)newSampleRate
 {
-    NSLog(@"setting sample rate: %f", newSampleRate);
+//    NSLog(@"setting sample rate: %f", newSampleRate);
     
     // OSMOCOM RTL-SDR DERIVED CODE
     uint16_t tmp;
@@ -1390,7 +1398,7 @@ void asyncCallback(void *refcon, IOReturn kretval, void *arg0)
                                             object:nil];
     [asyncThread start];
     
-    NSLog(@"Added asynchronous event source");
+//    NSLog(@"Added asynchronous event source");
     
     asyncBlock = inBlock;
     firstContext.device = self;
